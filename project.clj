a0723cda8d370cea42748590aa15fb727c4ba013
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-08-22 12:13:29 +0200

    Reformatted project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-         									;	https://clojure.org/api/api
          								[org.vladimir/mongo-lib "0.1.0"]
          								[org.vladimir/ajax-lib "0.1.0"]
          								]
