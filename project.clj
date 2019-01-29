6c81b3b3fb39611bf54b6ed5e4a8d47f16863226
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-01-29 17:54:25 +0100

    Task 30: Implemented chat on common (client, middle, server) projects

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.vladimirmarkovic86/session-lib "0.2.6"
+(defproject org.clojars.vladimirmarkovic86/session-lib "0.2.7"
   :description "Session library"
   :url "http://github.com/VladimirMarkovic86/session-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.3"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.2"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.2"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.4"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.3"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.3"]
                  ]
 
   :min-lein-version "2.0.0"
