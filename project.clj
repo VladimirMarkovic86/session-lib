2bcd3de4e118b65e551fd0680c3af32f43760ae0
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-04-04 17:59:50 +0200

    Task63: Implemented server-lib.core namespace tests and fixed some bugs.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/session-lib "0.2.16"
+(defproject org.clojars.vladimirmarkovic86/session-lib "0.2.17"
   :description "Session library"
   :url "http://github.com/VladimirMarkovic86/session-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.7"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.8"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.9"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
                  ]
 
