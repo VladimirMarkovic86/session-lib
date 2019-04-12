78d9d96e9f989ec4e904f41bee372e668811381f
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-04-12 16:11:03 +0200

    Task 65: Implemented pdf latex reports.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.vladimirmarkovic86/session-lib "0.2.17"
+(defproject org.clojars.vladimirmarkovic86/session-lib "0.2.18"
   :description "Session library"
   :url "http://github.com/VladimirMarkovic86/session-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.7"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.8"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.9"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.6"]
                  ]
 
   :min-lein-version "2.0.0"
