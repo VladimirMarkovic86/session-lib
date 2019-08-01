1702f226f72b03cd90319232d24aded76d0a9fbd
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-08-01 13:17:39 +0200

    Task 95: Refactored the way routing works.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/session-lib "0.2.29"
+(defproject org.clojars.vladimirmarkovic86/session-lib "0.2.30"
   :description "Session library"
   :url "http://github.com/VladimirMarkovic86/session-lib"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.13"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.14"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.11"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.12"]
                  ]
 
   :min-lein-version "2.0.0"
